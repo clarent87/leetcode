03a494017e5c978a8238764622362b26797b8e2f
clarent87 <45042053+clarent87@users.noreply.github.com>
2023-04-05 00:41:39 +0900

    Time: 102 ms (63.06%), Space: 17.1 MB (82.84%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,8 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        answer = defaultdict(list)
+
+        for word in strs:
+            answer[''.join(sorted(word))].append(word)
+
+        return list(answer.values())
