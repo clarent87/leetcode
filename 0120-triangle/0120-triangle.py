ed50e14739b990ee838fcf3331acfb46b7a8ba2b
clarent87 <45042053+clarent87@users.noreply.github.com>
2023-05-11 23:55:21 +0900

    Time: 86 ms (11.37%), Space: 17.7 MB (6.05%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,13 +1,13 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
 
-        memo = [[-1] * len(n) for n in triangle]
+        memo = [[None] * len(n) for n in triangle]
         max_level = len(triangle)-1
 
         def min_path(index: int, level: int) -> int:
             if level == max_level:
                 return triangle[level][index]
-            if memo[level][index] != -1:
+            if memo[level][index] != None:
                 return memo[level][index]
 
             memo[level][index] = triangle[level][index] + min(min_path(index, level + 1), min_path(index + 1, level + 1))
