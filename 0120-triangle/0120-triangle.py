03ea56ef9de5d21dd608d44069260413ea8718ca
clarent87 <45042053+clarent87@users.noreply.github.com>
2023-05-11 23:50:47 +0900

    Time: 83 ms (16.77%), Space: 17.8 MB (6.05%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,15 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+
         memo = [[-1] * len(triangle) for _ in range(len(triangle))]
-        def dfs(i, j):
-            if i == len(triangle):
-                return 0
-            if memo[i][j] != -1:
-                return memo[i][j]
+        max_level = len(triangle)-1
 
-            lower_left = triangle[i][j] + dfs(i + 1, j)
-            lower_right = triangle[i][j] + dfs(i + 1, j + 1)
-            memo[i][j] = min(lower_left, lower_right)
-            return memo[i][j]
+        def min_path(index: int, level: int) -> int:
+            if level == max_level:
+                return triangle[level][index]
+            if memo[level][index] != -1:
+                return memo[level][index]
 
-        return dfs(0, 0)
+            memo[level][index] = triangle[level][index] + min(min_path(index, level + 1), min_path(index + 1, level + 1))
+            return memo[level][index]
+
+        return min_path(0, 0)
