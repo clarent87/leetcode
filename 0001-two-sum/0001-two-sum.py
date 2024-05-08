8b28df0989a01d08bc9076456107d80d13b89ae7
clarent87 <45042053+clarent87@users.noreply.github.com>
2024-05-09 00:13:18 +0900

    Time: 53 ms (87.52%), Space: 17.9 MB (9.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,17 +1,31 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         pf, pe = 0, len(nums) - 1
+#         tmp = list()
+
+#         for i, v in enumerate(nums):
+#             tmp.append((i,v))
+
+#         tmp.sort(key=lambda x: x[1])
+#         while pf < pe:
+#             sum = tmp[pf][1] + tmp[pe][1]
+#             if sum == target:
+#                 return [tmp[pf][0], tmp[pe][0]]
+#             elif sum > target:
+#                 pe -= 1
+#             else:  # target보다 sum이 작음
+#                 pf += 1
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        pf, pe = 0, len(nums) - 1
-        tmp = list()
+        """풀이
+        1. 이중 반복.
+        2. hash
+        """
+        # 1. dict 만들기
+        map = dict(zip(nums, range(len(nums))))
 
-        for i, v in enumerate(nums):
-            tmp.append((i,v))
-
-        tmp.sort(key=lambda x: x[1])
-        while pf < pe:
-            sum = tmp[pf][1] + tmp[pe][1]
-            if sum == target:
-                return [tmp[pf][0], tmp[pe][0]]
-            elif sum > target:
-                pe -= 1
-            else:  # target보다 sum이 작음
-                pf += 1
+        # 2. 빼서 구하기
+        for i, e in enumerate(nums):
+            index = target - e
+            if index in map and map[index] is not i:
+                return [i, map[index]]
